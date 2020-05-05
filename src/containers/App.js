b213ef63f86d04de5a0716287c97bad34f34119d
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-05 14:46:31 -0500

    pretty please2

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     }
   }
 
-        componentDidMount() {
+          componentDidMount() {
             fetch('https://jsonplaceholder.typicode.com/users')
     .then(response=> response.json())
         .then(users => {this.setState({ robots: users})});
